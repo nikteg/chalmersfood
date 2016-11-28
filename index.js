9a88192fe680cf93014fe5a0610123ae5c21af0a
Niklas <me@sodapop.se>
2016-11-28 22:22:31 +0100

    Add last updated

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const cheerio = require("cheerio");
 const request = require("request");
 const async = require("async");
+const moment = require("moment");
 
 const app = express();
 
 const CACHE_LIFE = 1 * 60 * 60 * 1000;
-const cache = { date: Date.now(), data: null };
 
 const restaurants = [
   {
@@ -88,8 +88,48 @@ app.use("/static", express.static(`${__dirname}/public`));
 let clients = [];
 let isFetching = false;
 
+const validResult = (oldResult, result) => {
+
+  // If the old result contains an error, just use the new one
+  if (oldResult.error) {
+    return result;
+  }
+
+  if (result.error) {
+    return oldResult;
+  }
+
+  // Shallow compare on the items string arrays
+  for (let i = 0; i < result.items.length; ++i) {
+    if (result.items[i] !== oldResult.items[i]) {
+      return oldResult;
+    }
+  }
+
+  return result;
+}
+
+const success = (restaurant, items) => ({
+  items,
+  name: restaurant.name,
+  date: Date.now(),
+});
+
+const fail = (restaurant, error) => ({
+  error,
+  name: restaurant.name,
+  items: [],
+  date: Date.now(),
+});
+
+const cache = {
+  date: 0,
+  data: restaurants.map(restaurant =>
+    fail(restaurant, "Not loaded")),
+};
+
 const getData = () => new Promise((resolve, reject) => {
-  if (cache.data && Date.now() < cache.date + CACHE_LIFE) {
+  if (Date.now() < cache.date + CACHE_LIFE) {
     return resolve(cache.data);
   }
 
@@ -104,35 +144,29 @@ const getData = () => new Promise((resolve, reject) => {
     request(r.url, (err, resp, body) => {
       if (!err && resp.statusCode === 200) {
         try {
-          return cb(null, {
-            name: r.name,
-            items: r.parse(cheerio.load(body, {
-              xmlMode: r.format === "text/xml",
-            })),
-          });
+          const items = r.parse(cheerio.load(body, {
+            xmlMode: r.format === "text/xml",
+          }));
+
+          return process.nextTick(cb, null, success(r, items));
         } catch (e) {
-          return cb(null, {
-            name: r.name,
-            items: [],
-            error: "Could not parse",
-          });
+          return cb(null, fail(r, "Could not parse"));
         }
       }
 
-      return cb(null);
+      return cb(null, fail(r, "Could not fetch"));
     });
   }, (err, results) => {
-    const data = results.filter(r => r != null);
-
     console.log("Setting new cache!");
 
     cache.date = Date.now();
-    cache.data = data;
+    cache.data = results.map((result, i) =>
+      validResult(cache.data[i], result));
 
     isFetching = false;
 
     console.log("Resolving new data to", clients.length, "clients");
-    clients.map(f => f(data));
+    clients.map(resolveFunc => resolveFunc(cache.data));
 
     clients = [];
   });
@@ -144,16 +178,20 @@ const filterDay = (data, dayIndex) => data.map(r => {
   return Object.assign({}, r, { items });
 });
 
+const formatDate = (restaurant) =>
+  Object.assign(restaurant, { date: moment(restaurant.date).fromNow() });
+
 app.get("/", (req, res) => {
   const day = new Date();
   const today = Math.min(Math.max(0, day.getDay() - 1), 4);
   const selectedDay = req.query.day || today;
 
   getData().then(data => res.render("index", {
-    data: filterDay(data, selectedDay),
+    data: filterDay(data, selectedDay).map(formatDate),
     selectedDay,
   }));
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Chalmersfood listening on port ${port}!`));
+app.listen(port, () =>
+  console.log(`Chalmersfood listening on http://localhost:${port}/ !`));
