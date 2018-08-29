d57fff64c3c3c91c4029a290875718bec7c63010
Niklas <me@sodapop.se>
2018-08-29 16:57:23 +0200

    Fix einstein, get rid of cheerio

--- src/restaurants.ts
+++ src/restaurants.ts
@@ -1,4 +1,5 @@
 import { flatMap } from "./utils";
+import { JSDOM } from "jsdom";
 
 export interface BaseRestaurant {
   name: string;
@@ -48,7 +49,6 @@ export namespace CarbonCloud {
   export interface RestaurantInput {
     menus: Menu[];
   }
-
 }
 
 export interface JSONRestaurant<T> extends BaseRestaurant {
@@ -58,39 +58,53 @@ export interface JSONRestaurant<T> extends BaseRestaurant {
 
 export interface HTMLRestaurant extends BaseRestaurant {
   format: HTMLFormat;
-  map: ($: CheerioStatic) => string[][];
+  map: (jsdom: JSDOM) => string[][];
 }
 
-export interface XMLRestaurant extends BaseRestaurant {
-  format: XMLFormat;
-  map: ($: CheerioStatic) => string[][];
-}
+// export interface XMLRestaurant extends BaseRestaurant {
+//   format: XMLFormat;
+//   map: ($: CheerioStatic) => string[][];
+// }
 
 export interface PlainRestaurant extends BaseRestaurant {
   format: PlainFormat;
   map: (text: string) => string[][];
 }
 
-export type Restaurant = JSONRestaurant<any> | HTMLRestaurant | XMLRestaurant | PlainRestaurant;
+export type Restaurant =
+  | JSONRestaurant<any>
+  | HTMLRestaurant
+  // | XMLRestaurant
+  | PlainRestaurant;
 
-export function jsonRestaurant<T>(name: string, url: string, map: (input: T) => string[][]): JSONRestaurant<T> {
+export function jsonRestaurant<T>(
+  name: string,
+  url: string,
+  map: (input: T) => string[][]
+): JSONRestaurant<T> {
   return {
     name,
     url,
     format: "application/json",
-    map,
-  }
+    map
+  };
 }
 
 function displayRecipeCategory(category: CarbonCloud.RecipeCategory) {
-  return category.recipes.map((recipe) => `${category.name} – ${recipe.displayNames[0].displayName}`)
+  return category.recipes.map(
+    recipe => `${category.name} – ${recipe.displayNames[0].displayName}`
+  );
 }
 
 export const restaurants = [
   jsonRestaurant<CarbonCloud.RestaurantInput>(
     "Kårresturangen",
     "http://carboncloudrestaurantapi.azurewebsites.net/api/menuscreen/getdataweek?restaurantid=5",
-    (json) => json.menus.map((menu) => flatMap(displayRecipeCategory, menu.recipeCategories))),
+    json =>
+      json.menus.map(menu =>
+        flatMap(displayRecipeCategory, menu.recipeCategories)
+      )
+  ),
   // jsonRestaurant<CarbonCloud.RestaurantInput>(
   //   "Linsen",
   //   "http://carboncloudrestaurantapi.azurewebsites.net/api/menuscreen/getdataweek?restaurantid=33",
@@ -98,26 +112,42 @@ export const restaurants = [
   jsonRestaurant<CarbonCloud.RestaurantInput>(
     "Express",
     "http://carboncloudrestaurantapi.azurewebsites.net/api/menuscreen/getdataweek?restaurantid=7",
-    (json) => json.menus.map((menu) => flatMap(displayRecipeCategory, menu.recipeCategories))),
+    json =>
+      json.menus.map(menu =>
+        flatMap(displayRecipeCategory, menu.recipeCategories)
+      )
+  ),
   jsonRestaurant<CarbonCloud.RestaurantInput>(
     "S.M.A.K.",
     "http://carboncloudrestaurantapi.azurewebsites.net/api/menuscreen/getdataweek?restaurantid=42",
-    (json) => json.menus.map((menu) => flatMap(displayRecipeCategory, menu.recipeCategories))),
+    json =>
+      json.menus.map(menu =>
+        flatMap(displayRecipeCategory, menu.recipeCategories)
+      )
+  ),
   <HTMLRestaurant>{
     name: "Einstein",
-    url: "http://www.butlercatering.se/print/11",
+    url: "http://restaurang-einstein.se/",
     format: "text/html",
-    map: ($) => $(".node-lunchmeny .content .field-day").get().map((day) => {
-      return $(day).find("p")
-        .map((j, d) => d.children[0])
-        .filter((j, d) => {
-          const t = (d as any).data.trim();
+    map: jsdom => {
+      return [
+        ...jsdom.window.document.querySelectorAll(
+          "#column_gnxhsuatx .content-wrapper .column-content"
+        )
+      ]
+        .filter((_, i) => i % 2 == 1)
+        .map(elem => {
+          const ps = [...elem.querySelectorAll("p")];
 
-          // \u200B is a Unicode zero-width space
-          return t !== "" && t !== "\u200B";
-        })
-        .map((j, d) => (d as any).data.trim())
-        .get();
-    }),
-  },
+          if (ps.length === 0) return [];
+
+          return flatMap(
+            v => v,
+            flatMap(p => [...p.childNodes], [...ps])
+              .filter((node: Node) => node.nodeType === 3)
+              .map((node: Node) => node.textContent!.trim())
+          ).filter((text: string) => text !== "");
+        });
+    }
+  }
 ];
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
-import * as cheerio from "cheerio";
 import { Restaurant } from "restaurants";
+import { JSDOM, VirtualConsole } from "jsdom";
 
 export function flatMap(f: (...args: any[]) => any, xs: any[]) {
   return xs.reduce((acc, x) => acc.concat(f(x)), []);
@@ -11,10 +11,15 @@ export function weekOfYear(date: Date) {
   d.setHours(0, 0, 0);
   d.setDate(d.getDate() + 4 - (d.getDay() || 7));
 
-  return Math.ceil((((d.getTime() - new Date(d.getFullYear(), 0, 1).getTime()) / 8.64e7) + 1) / 7);
+  return Math.ceil(
+    ((d.getTime() - new Date(d.getFullYear(), 0, 1).getTime()) / 8.64e7 + 1) / 7
+  );
 }
 
-export function safeParseRestaurantBody(restaurant: Restaurant, body: string): string[][] {
+export function safeParseRestaurantBody(
+  restaurant: Restaurant,
+  body: string
+): string[][] {
   const result = parseRestaurantBody(restaurant, body);
 
   if (Array.isArray(result)) {
@@ -24,14 +29,16 @@ export function safeParseRestaurantBody(restaurant: Restaurant, body: string): s
   return [];
 }
 
+const virtualConsole = new VirtualConsole();
+virtualConsole.sendTo(console, { omitJSDOMErrors: true });
+
 function parseRestaurantBody(restaurant: Restaurant, body: string) {
   switch (restaurant.format) {
     case "application/json":
-      return restaurant.map(JSON.parse(body))
+      return restaurant.map(JSON.parse(body));
     case "text/html":
-    case "text/xml":
-      return restaurant.map(cheerio.load(body, { xmlMode: restaurant.format === "text/html" }));
+      return restaurant.map(new JSDOM(body, { virtualConsole }));
     default:
       return restaurant.map(body);
   }
-};
+}
